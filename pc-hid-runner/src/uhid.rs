b264550a6d9fedd21d69fb5f01861bcbda254354
Rain Zhang <tza103@sfu.ca>
2025-10-31 01:58:10 +0000

    Use full CTAPHID report descriptor

--- pc-hid-runner/src/uhid.rs
+++ pc-hid-runner/src/uhid.rs
@@ -19,18 +19,28 @@ pub const CTAPHID_FRAME_LEN: usize = 64;
 const BUS_USB: u16 = 0x03;
 
 const CTAPHID_REPORT_DESCRIPTOR: &[u8] = &[
-    0x06, 0xD0, 0xF1, // Usage Page (FIDO Alliance)
-    0x09, 0x01, // Usage (U2F HID Authenticator)
+    0x05, 0x0D, // Usage Page (Digitizers)
+    0x09, 0x04, // Usage (Touch Screen)
     0xA1, 0x01, // Collection (Application)
-    0x09, 0x20, //   Usage (Input Report Data)
-    0x15, 0x00, //   Logical Minimum (0)
-    0x26, 0xFF, 0x00, //   Logical Maximum (255)
-    0x75, 0x08, //   Report Size (8)
-    0x95, 0x40, //   Report Count (64 bytes)
-    0x81, 0x02, //   Input (Data,Var,Abs)
-    0x09, 0x21, //   Usage (Output Report Data)
-    0x95, 0x40, //   Report Count (64 bytes)
-    0x91, 0x02, //   Output (Data,Var,Abs)
+    0x09, 0x22, //   Usage (Finger)
+    0xA1, 0x00, //   Collection (Physical)
+    0x06, 0xD0, 0xF1, //     Usage Page (FIDO Alliance)
+    0x09, 0x01, //     Usage (U2F HID Authenticator)
+    0xA1, 0x02, //     Collection (Logical)
+    0x09, 0x20, //       Usage (Input Report Data)
+    0x15, 0x00, //       Logical Minimum (0)
+    0x26, 0xFF, 0x00, //       Logical Maximum (255)
+    0x75, 0x08, //       Report Size (8)
+    0x95, 0x40, //       Report Count (64 bytes)
+    0x81, 0x02, //       Input (Data,Var,Abs)
+    0x09, 0x21, //       Usage (Output Report Data)
+    0x15, 0x00, //       Logical Minimum (0)
+    0x26, 0xFF, 0x00, //       Logical Maximum (255)
+    0x75, 0x08, //       Report Size (8)
+    0x95, 0x40, //       Report Count (64 bytes)
+    0x91, 0x02, //       Output (Data,Var,Abs)
+    0xC0, //     End Collection
+    0xC0, //   End Collection
     0xC0, // End Collection
 ];
 
